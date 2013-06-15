e53b77f905ff2325df5de3879809fc8dabfa2b25
Yaroslav Klymko <t3hnar@gmail.com>
2013-06-15 14:53:26 +0300

    minor improvements

--- common/src/main/scala/com/thenewmotion/ocpp/ActionNotSupportedException.scala
+++ common/src/main/scala/com/thenewmotion/ocpp/ActionNotSupportedException.scala
@@ -4,4 +4,4 @@ package com.thenewmotion.ocpp
  * @author Yaroslav Klymko
  */
 case class ActionNotSupportedException(v: Version.Value, action: String)
-  extends Exception("Action '%s' is not supported in ocpp %s".format(action, v))
+  extends Exception(s"Action '$action' is not supported in ocpp $v")
