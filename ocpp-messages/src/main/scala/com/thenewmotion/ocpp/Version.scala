df51bbd60a6ef2124efc1cad2e3516fb9df0993d
Reinier Lamers <r.lamers@thenewmotion.com>
2017-09-27 17:23:17 +0200

    TNM-4962 make it possible to use both 1.5 and 1.6 with the OcppJsonClient interface

--- ocpp-messages/src/main/scala/com/thenewmotion/ocpp/Version.scala
+++ ocpp-messages/src/main/scala/com/thenewmotion/ocpp/Version.scala
@@ -8,5 +8,6 @@ object Version extends Enumerable[Version] {
   case object V12 extends Version
   case object V15 extends Version
   case object V16 extends Version
+
   val values = Set(V12, V15, V16)
 }
